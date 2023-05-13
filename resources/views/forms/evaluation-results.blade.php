1721135077768024ebb48822f58d75a7118f3c76
RazanAbuhamda <rrazanramii@gmail.com>
2023-05-13 21:08:34 +0300

    display evaluation results with average grade

--- resources/views/forms/evaluation-results.blade.php
+++ resources/views/forms/evaluation-results.blade.php
@@ -19,6 +19,11 @@
     @endif
 
     @if ($evaluationResults && !$evaluationResults->isEmpty())
+        @php
+            $totalResults = count($evaluationResults);
+            $totalGrade = 0;
+        @endphp
+
         @foreach ($evaluationResults as $result)
             <div class="card mb-4">
                 <br><br>
@@ -31,12 +36,14 @@
                             <th>Topic Name</th>
                             <th>Question</th>
                             <th>Answer</th>
+                            <th></th>
+                            <th></th>
                             <th>Score</th>
                         </tr>
                         @foreach ($result->result_json['form'] as $section)
                             <tr>
                                 <td rowspan="{{ count($section['questions']) + 1 }}">{{ $section['name'] }}</td>
-                            </tr><br>
+                            </tr>
                             @foreach ($section['questions'] as $question)
                                 <tr>
                                     <td>{{ $question['title'] }}</td>
@@ -49,34 +56,55 @@
                                             {{ $question['answer'] }}
                                         @endif
                                     </td>
+                                    <td></td>
+                                    <td></td>
                                     <td>{{ $question['credit'] }}</td>
                                 </tr>
-
                             @endforeach
                             <tr>
-                                <td colspan="3" align="right"><strong>Total Credit:</strong></td>
-                                <td>{{ $section['credit'] }}</td>
+                                <td colspan="6"><strong>Topic report</strong></td>
                             </tr>
-                            <tr>
-                                <td colspan="3" align="right"><strong>Total Weight:</strong></td>
-                                <td>{{ $section['weight'] }}</td>
-                            </tr>
-                            <tr>
-                                <td colspan="3" align="right"><strong>Result Grade:</strong></td>
-                                <td>{{ $result->result_json['result_grade'] }}</td>
-                            </tr>
+{{--                            <tr>--}}
+{{--                                <td></td>--}}
+{{--                                <td colspan="3" align="right" style="border: 1px solid grey;"><strong>Topic Total--}}
+{{--                                        Credit:</strong></td>--}}
+{{--                                <td style="border: 1px solid black;">{{ $section['credit'] }}</td>--}}
+{{--                            </tr>--}}
+{{--                            <tr>--}}
+{{--                                <td></td>--}}
+{{--                                <td colspan="3" align="right" style="border: 1px solid grey;"><strong>Topic Total--}}
+{{--                                        Weight:</strong></td>--}}
+{{--                                <td style="border: 1px solid black;">{{ $section['weight'] }}</td>--}}
+{{--                            </tr>--}}
+
                             <tr style="height: 40px;"></tr>
                             <tr style="height: 20px;"></tr>
                         @endforeach
-
+                        <tr>
+                            <td></td>
+                            <td colspan="3" align="right" style="border: 1px solid black;"><strong>Form Result
+                                    Grade:</strong></td>
+                            <td style="border: 1px solid black;">{{ $result->result_json['result_grade'] }}</td>
+                        </tr>
                         <tr style="height: 40px;"></tr>
                     </table>
                     <hr>
                 </div>
             </div>
+            @php
+                $totalGrade += $result->result_json['result_grade'];
+            @endphp
         @endforeach
 
+        @php
+            $averageGrade = ($totalResults > 0) ? ($totalGrade / $totalResults) : 0;
+        @endphp
+
+        <div class="mt-4">
+            <h3 align="right">Average Form Results Grade: {{ $averageGrade }}%</h3>
+        </div>
     @else
         <p>No evaluation results found.</p>
     @endif
 @endsection
+
