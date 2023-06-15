318fb2b9ae9bb88c06a54e0d64c4e45c0a37f99a
RazanAbuhamda <rrazanramii@gmail.com>
2023-06-15 22:33:24 +0300

    login style

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,43 +1,55 @@
 @extends('layouts.auth-layout')
+
 @section('auth-body')
-    <body>
-    <div class="login-content">
-        <!-- Login -->
+    <body style=" background-color: #F7C049;">
+    <div class="login-content" style="background-color: #F7C049; display: flex; flex-direction: column;margin: -180px">
+        <!-- Logo -->
+        <div class="logo-area" style="">
+            <a href="#"><img src="{{ asset('dashboardPublic/img/logoAuth.jpeg') }}" alt="Logo" class="logo-img"></a>
+        </div>
         <div class="nk-block toggled" id="l-login">
-            <form id="login-form" method="POST" action="{{ route('login') }}">
+            <div class="form-header" style="position: fixed; top: 0; left: 0; width: 400px; border-radius:10px; padding: 30px; background-color: #FFFFFF; margin-left: 50px ">
+            <h4>Sign In</h4><hr>
+            </div>
+            <form id="login-form" method="POST" action="{{ route('login') }}" style="width: 400px; height:550px; padding: 20px; background-color: #FFFFFF; border-radius: 10px;margin-left: 50px">
                 @csrf
-                <div class="nk-form">
-                    <div class="input-group">
-                        <span class="input-group-addon nk-ic-st-pro"><i class="notika-icon notika-support"></i></span>
-                        <div class="nk-int-st">
-                            <input type="text" name="email" class="form-control" placeholder="Email">
+                <div class="nk-form" style="margin-top: 40px">
+                    <div class="input-group" style="margin-bottom: 15px;">
+                        <div class="input-group" style="margin-bottom: 15px;">
+                            <div class="nk-int-st">
+                                <label style="float: left">Email</label>
+                                <input type="text" name="email" class="form-control rounded" placeholder="Email" style="height: 40px; padding-left: 40px; background-image: url('{{ asset('dashboardPublic/img/email-icon.png') }}'); background-repeat: no-repeat; background-position: left center;">
+                            </div>
                         </div>
+
                     </div>
-                    <div class="input-group mg-t-15">
-                        <span class="input-group-addon nk-ic-st-pro"><i class="notika-icon notika-edit"></i></span>
-                        <div class="nk-int-st">
-                            <input type="password" name="password" class="form-control" placeholder="Password">
+                    <div class="input-group" style="margin-bottom: 15px;">
+                        <div class="input-group" style="margin-bottom: 15px;">
+                            <div class="nk-int-st">
+                                <label style="float: left">Password</label>
+                                <input type="password" name="password" class="form-control rounded" placeholder="Password" style="height: 40px; padding-left: 40px; background-image: url('{{ asset('dashboardPublic/img/password-icon.png') }}'); background-repeat: no-repeat; background-position: left center;">
+                            </div>
                         </div>
+
                     </div>
-                    <div class="fm-checkbox">
+                    <div class="fm-checkbox" style="margin-bottom: 15px;">
                         <label><input type="checkbox" class="i-checks"> <i></i> Keep me signed in</label>
                     </div>
-                    <a href="{{ route('register') }}" data-ma-action="nk-login-switch" data-ma-block="#l-register"
-                       class="btn btn-login btn-success btn-float"><i
-                            class="notika-icon notika-right-arrow right-arrow-ant"></i></a>
+                    <button class="btn btn-primary rounded" style="background-color: #F7C049; border-radius: 50px;border-color:#F7C049; width: 200px;height: 40px; font-size: 15px">Sign In</button>
+                    <div class="form-footer" style="position: fixed; bottom: 0; left: 0; width: 400px; border-radius:10px; padding: 30px; background-color: #FFFFFF; text-align: center;margin-left: 50px">
+                        <span style="margin-right: 10px">you don't have an account?</span>
+                        <a href="{{ route('register') }}">Sign Up</a>
+                    </div>
                 </div>
 
-                <div class="nk-navigation nk-lg-ic">
-                    <a href="{{ route('register') }}" data-ma-action="nk-login-switch" data-ma-block="#l-register"><i
-                            class="notika-icon notika-plus-symbol"></i> <span>Register</span></a>
+                <div class="nk-navigation nk-lg-ic" style="margin-top: 15px;">
+                    <a href="{{ route('register') }}" data-ma-action="nk-login-switch" data-ma-block="#l-register"><i class="notika-icon notika-plus-symbol"></i> <span>Register</span></a>
                     @if (Route::has('password.request'))
                         <a href="{{ route('password.request') }}" data-ma-action="nk-login-switch" data-ma-block="#l-forget-password"><i>?</i> <span>Forgot Password</span></a>
                     @endif
                 </div>
-                <button>login</button>
             </form>
         </div>
     </div>
     </body>
-
 @endsection
--- resources/views/sub-views/auth-header.blade.php
+++ resources/views/sub-views/auth-header.blade.php
@@ -1,15 +1,43 @@
 <!doctype html>
 <html class="no-js" lang="">
-
 <head>
+    <style>
+        html, body {
+            font-family: 'Roboto', sans-serif;
+            font-weight: 400;
+
+            box-sizing: border-box;
+        }
+        .logo-img {
+            max-width: 200px; /* Adjust the width as needed */
+            height: 100px; /* Maintain the aspect ratio */
+            align-items: center;
+
+        }
+        .delete-button {
+            background-color: #FFFFFF;
+            color: red;
+        }
+        .pull-left {
+            display: flex;
+            align-items: center;
+        }
+
+        .pull-left i {
+            margin-right: 35px; /* Adjust the spacing between the icon and the text */
+        }
+        .fa-angle-left {
+            font-size: 24px; /* تعديل حجم السهم حسب الحجم المطلوب */
+        }
+    </style>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Third-party evaluation system</title>
+    <title>Rate Mentor System</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- favicon
 		============================================ -->
-    <link rel="shortcut icon" type="image/x-icon" href={{asset('dashboardPublic/assets/img/favicon.ico')}}>
+    <link rel="shortcut icon" type="image/x-icon" href={{asset('dashboardPublic/assets/img/favicon.png')}}>
     <!-- Google Fonts
 		============================================ -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900" rel="stylesheet">
@@ -51,6 +79,7 @@
     <link rel="stylesheet" href={{asset('style.css')}}>
     <!-- responsive CSS
 		============================================ -->
+
     <link rel="stylesheet" href={{asset('dashboardPublic/assets/css/responsive.css')}}>
     <!-- modernizr JS
 		============================================ -->
